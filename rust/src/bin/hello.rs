8d1d52a4a33e007d6ce554e482e44c62902f9259
xxchan <xxchan22f@gmail.com>
2023-06-11 20:12:51 +0200

    mv rust

--- rust/src/bin/hello.rs
+++ rust/src/bin/hello.rs
@@ -2,7 +2,7 @@
 async fn main() {
     println!("Hello async world!");
     // panic!("Oh no!")
-    let x = [1];
+    let x = r[1];
     // trigger a SIGSEGV
     unsafe {
         println!("{}", x.get_unchecked(10000000));
